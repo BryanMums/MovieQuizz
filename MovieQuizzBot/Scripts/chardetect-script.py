ce59f31cae6b17112803b7ab9c7f0de8cd8f3e87
bryan.muhmenth <bryan.muhmenthaler@he-arc.ch>
2016-04-27 15:42:10 +0200

    Ajout d'un bot de test
    Un bot de test a été ajouté avec le code d'exemple donné.

---
+++ MovieQuizzBot/Scripts/chardetect-script.py
@@ -0,0 +1,10 @@
+#!d:\2eme_annee\2241-langages_et_frameworks\python\moviequizz\moviequizzbot\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'chardet==2.3.0','console_scripts','chardetect'
+__requires__ = 'chardet==2.3.0'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('chardet==2.3.0', 'console_scripts', 'chardetect')()
+    )
